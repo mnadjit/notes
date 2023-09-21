d5e62e3af1788cd4ea3c11b1bfb924c6736a22b2
Mehdi <m.nadji.t@gmail.com>
2023-09-21 20:48:09 +1000

    updated

--- 🏢work/nh/Scripts/PowerShell/save_to_git/load.ps1
+++ 🏢work/nh/Scripts/PowerShell/save_to_git/load.ps1
@@ -3,9 +3,11 @@
 $script:args | % {
 	cd $_
 	echo "`nDownloading from origin into path: $($_)`n"
-	git fetch --all
-	git reset --hard origin/main
-	git pull --force
+	git status
+	Read-Host "Continue?"
+	## git fetch --all
+	## git reset --hard origin/main
+	git pull
 }
 
 cd $cur_dir
